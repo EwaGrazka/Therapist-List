c0a19cad01712e7a820d6e495899f0d2747c2faa
Ewa Grązka <fk.grazka@gmail.com>
2021-01-31 00:51:41 +0100

    handle images

--- src/js/List.js
+++ src/js/List.js
@@ -3,6 +3,8 @@ import "../scss/Lista.scss";
 import Pagination from "@material-ui/lab/Pagination";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { ReactComponent as Logo } from "./../img/logo.svg";
+import { ReactComponent as CloseButton } from "./../img/closeButton.svg";
 
 import { connect } from "react-redux";
 import {
@@ -22,7 +24,7 @@ class List extends Component {
     isEditSideBarOpen: false,
     pageIndex: 0,
     pagination: {
-      pageCount: 36,
+      pageCount: 0,
       startIndex: 0,
       endIndex: 10,
     },
@@ -30,19 +32,22 @@ class List extends Component {
   handleInitialState = () => {
     this.setState({
       pagination: {
-        pageCount: Math.ceil(this.props.therapists.length / 10),
+        pageCount: Math.floor(this.props.therapists.length / 10),
+        startIndex: this.state.pagination.startIndex,
+        endIndex: this.state.pagination.endIndex,
       },
     });
   };
 
   componentDidMount() {
     this.props.fetchTherapists();
-    if (this.props.therapists) {
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.therapists !== prevProps.therapists) {
       this.handleInitialState();
     }
   }
-
-  // notify = () => toast("Pomyślnie zmieniono dane.");
 
   handleClick = (id) => {
     this.setState({
@@ -134,19 +139,7 @@ class List extends Component {
       <>
         <ToastContainer />
         <header>
-          <svg
-            width="28"
-            height="28"
-            className="logo"
-            viewBox="0 0 27 16"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M7.89551 16H4.82324V3.67871H0.193359V0.595703H12.5146V3.67871H7.89551V16ZM17.4305 3.67871V9.84473H20.5135C20.936 9.84473 21.3335 9.76595 21.7059 9.6084C22.0783 9.44368 22.4041 9.22168 22.6834 8.94238C22.9627 8.66309 23.1811 8.33724 23.3387 7.96484C23.5034 7.58529 23.5857 7.18424 23.5857 6.76172C23.5857 6.33919 23.5034 5.94173 23.3387 5.56934C23.1811 5.18978 22.9627 4.86035 22.6834 4.58105C22.4041 4.30176 22.0783 4.08333 21.7059 3.92578C21.3335 3.76107 20.936 3.67871 20.5135 3.67871H17.4305ZM17.4305 16H14.3475V0.595703H20.5135C21.0792 0.595703 21.6235 0.670898 22.1463 0.821289C22.6691 0.964518 23.1561 1.1722 23.6072 1.44434C24.0656 1.70931 24.4809 2.03158 24.8533 2.41113C25.2329 2.78353 25.5551 3.19889 25.8201 3.65723C26.0923 4.11556 26.2999 4.60612 26.4432 5.12891C26.5936 5.65169 26.6687 6.19596 26.6687 6.76172C26.6687 7.60677 26.5076 8.40527 26.1854 9.15723C25.8631 9.90202 25.4227 10.5537 24.8641 11.1123C24.3055 11.6709 23.6502 12.1113 22.8982 12.4336C22.1535 12.7559 21.3585 12.917 20.5135 12.917H17.4305V16Z"
-              fill="#213C9A"
-            />
-          </svg>
+          <Logo />
           <h1>Lista specjalistów</h1>
         </header>
 
@@ -155,76 +148,69 @@ class List extends Component {
             <div className="bcg">
               {" "}
               <div className="sideBar">
-                <span onClick={() => this.closeSideBar()} className="x">
-                  <svg
-                    width="14"
-                    height="14"
-                    viewBox="0 0 14 14"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      d="M13.3 0.709987C13.1131 0.522734 12.8595 0.417501 12.595 0.417501C12.3305 0.417501 12.0768 0.522734 11.89 0.709987L6.99997 5.58999L2.10997 0.699987C1.92314 0.512734 1.66949 0.407501 1.40497 0.407501C1.14045 0.407501 0.886802 0.512734 0.699971 0.699987C0.309971 1.08999 0.309971 1.71999 0.699971 2.10999L5.58997 6.99999L0.699971 11.89C0.309971 12.28 0.309971 12.91 0.699971 13.3C1.08997 13.69 1.71997 13.69 2.10997 13.3L6.99997 8.40999L11.89 13.3C12.28 13.69 12.91 13.69 13.3 13.3C13.69 12.91 13.69 12.28 13.3 11.89L8.40997 6.99999L13.3 2.10999C13.68 1.72999 13.68 1.08999 13.3 0.709987Z"
-                      fill="#707A88"
-                    />
-                  </svg>
-                </span>
+                <div className="sideBarHeader">
+                  <span onClick={() => this.closeSideBar()} className="x">
+                    <CloseButton />
+                  </span>
+                  <h1 className="info">Informacja o specjaliście</h1>
+                </div>
+
                 {!isEditSideBarOpen ? (
-                  <div>
-                    <h1 className="info">Informacja o specjaliście</h1>
+                  <>
+                    <div>
+                      <h3 className="name">Imię i nazwisko</h3>
 
-                    <h3 className="name">Imię i nazwisko</h3>
+                      {therapistDetails &&
+                      therapistDetails.fullName &&
+                      therapistDetails.fullName !== "" ? (
+                        <p>{therapistDetails.fullName}</p>
+                      ) : (
+                        <p>Brak danych</p>
+                      )}
 
-                    {therapistDetails &&
-                    therapistDetails.fullName &&
-                    therapistDetails.fullName !== "" ? (
-                      <p>{therapistDetails.fullName}</p>
-                    ) : (
-                      <p>Brak danych</p>
-                    )}
+                      <h3>Specjalizacje</h3>
+                      {therapistDetails &&
+                      therapistDetails.specializations &&
+                      therapistDetails.specializations.length !== 0 ? (
+                        <p>{therapistDetails.specializations.join(`, `)}</p>
+                      ) : (
+                        <p>Brak danych</p>
+                      )}
+                      <h3>Rodzaje terapii</h3>
+                      {therapistDetails &&
+                      therapistDetails.therapyTypes &&
+                      therapistDetails.therapyTypes.length !== 0 ? (
+                        <p>{therapistDetails.therapyTypes.join(`, `)}</p>
+                      ) : (
+                        <p>Brak danych</p>
+                      )}
+                      <h3>Certyfikaty</h3>
 
-                    <h3>Specjalizacje</h3>
-                    {therapistDetails &&
-                    therapistDetails.specializations &&
-                    therapistDetails.specializations.length !== 0 ? (
-                      <p>{therapistDetails.specializations.join(`, `)}</p>
-                    ) : (
-                      <p>Brak danych</p>
-                    )}
-                    <h3>Rodzaje terapii</h3>
-                    {therapistDetails &&
-                    therapistDetails.therapyTypes &&
-                    therapistDetails.therapyTypes.length !== 0 ? (
-                      <p>{therapistDetails.therapyTypes.join(`, `)}</p>
-                    ) : (
-                      <p>Brak danych</p>
-                    )}
-                    <h3>Certyfikaty</h3>
+                      {therapistDetails &&
+                      therapistDetails.certificates &&
+                      therapistDetails.certificates.length !== 0 ? (
+                        therapistDetails.certificates.map((therapist) => {
+                          return (
+                            <p>
+                              <li>{therapist.name}</li>
+                            </p>
+                          );
+                        })
+                      ) : (
+                        <p>Brak danych</p>
+                      )}
 
-                    {therapistDetails &&
-                    therapistDetails.certificates &&
-                    therapistDetails.certificates.length !== 0 ? (
-                      therapistDetails.certificates.map((therapist) => {
-                        return (
-                          <p>
-                            <li>{therapist.name}</li>
-                          </p>
-                        );
-                      })
-                    ) : (
-                      <p>Brak danych</p>
-                    )}
-
-                    <h3>O mnie</h3>
-                    {therapistDetails &&
-                    therapistDetails.aboutMe &&
-                    therapistDetails.aboutMe !== "" ? (
-                      <p className="aboutMe">
-                        {aboutMe || therapistDetails.aboutMe}
-                      </p>
-                    ) : (
-                      <p>Brak danych</p>
-                    )}
+                      <h3>O mnie</h3>
+                      {therapistDetails &&
+                      therapistDetails.aboutMe &&
+                      therapistDetails.aboutMe !== "" ? (
+                        <p className="aboutMe">
+                          {aboutMe || therapistDetails.aboutMe}
+                        </p>
+                      ) : (
+                        <p>Brak danych</p>
+                      )}
+                    </div>
                     <div className="mainButtonsWrapper">
                       <button
                         className="edit"
@@ -239,7 +225,7 @@ class List extends Component {
                         Usuń
                       </button>
                     </div>
-                  </div>
+                  </>
                 ) : (
                   <div className="editSideBarWrapper">
                     <h1>Edytuj informacje o specjaliście</h1>
